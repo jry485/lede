6360f12eb9ff784f00066009d9cca082f5a7f6de
W_Y_CPP <383152993@qq.com>
2021-12-29 23:37:08 +0900

    kernel:add kmod-lib-oid-registry

--- package/kernel/linux/modules/lib.mk
+++ package/kernel/linux/modules/lib.mk
@@ -272,3 +272,17 @@ define KernelPackage/asn1-decoder
 endef
 
 $(eval $(call KernelPackage,asn1-decoder))
+
+define KernelPackage/lib-oid-registry
+  SUBMENU:=$(LIB_MENU)
+  TITLE:=OID-Registry function support
+  KCONFIG:=CONFIG_OID_REGISTRY
+  FILES:=$(LINUX_DIR)/lib/oid_registry.ko
+  AUTOLOAD:=$(call AutoProbe,oid-registry)
+endef
+
+define KernelPackage/lib-oid-registry/description
+ Kernel module for OID-Registry function support
+endef
+
+$(eval $(call KernelPackage,lib-oid-registry))
