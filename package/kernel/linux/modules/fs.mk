32263deb3bc8d3553b1440efd0388aaa2e7b672e
W_Y_CPP <383152993@qq.com>
2022-01-06 23:17:04 -0500

    kmod-fs-nfs: adjust depends again

--- package/kernel/linux/modules/fs.mk
+++ package/kernel/linux/modules/fs.mk
@@ -398,7 +398,9 @@ $(eval $(call KernelPackage,fs-nfs))
 define KernelPackage/fs-nfs-ssc
   SUBMENU:=$(FS_MENU)
   TITLE:=Common NFS filesystem SSC Helper module
-  KCONFIG:= CONFIG_NFS_V4_2@ge5.15
+  KCONFIG:= \
+	CONFIG_GRACE_PERIOD@ge5.10 \
+	CONFIG_NFS_V4_2_SSC_HELPER=y@ge5.15
   FILES:= $(LINUX_DIR)/fs/nfs_common/nfs_ssc.ko@ge5.10
   AUTOLOAD:=$(call AutoLoad,30,nfs_ssc)
 endef
@@ -409,7 +411,7 @@ $(eval $(call KernelPackage,fs-nfs-ssc))
 define KernelPackage/fs-nfs-common
   SUBMENU:=$(FS_MENU)
   TITLE:=Common NFS filesystem modules
-  DEPENDS:=+LINUX_5_15:kmod-fs-nfs-ssc
+  DEPENDS:=+!LINUX_5_4:kmod-fs-nfs-ssc
   KCONFIG:= \
 	CONFIG_LOCKD \
 	CONFIG_SUNRPC \
@@ -417,8 +419,7 @@ define KernelPackage/fs-nfs-common
   FILES:= \
 	$(LINUX_DIR)/fs/lockd/lockd.ko \
 	$(LINUX_DIR)/net/sunrpc/sunrpc.ko \
-	$(LINUX_DIR)/fs/nfs_common/grace.ko \
-	$(LINUX_DIR)/fs/nfs_common/nfs_ssc.ko@ge5.10
+	$(LINUX_DIR)/fs/nfs_common/grace.ko
   AUTOLOAD:=$(call AutoLoad,30,grace sunrpc lockd)
 endef
 
@@ -474,7 +475,7 @@ $(eval $(call KernelPackage,fs-nfs-v3))
 define KernelPackage/fs-nfs-v4
   SUBMENU:=$(FS_MENU)
   TITLE:=NFS4 filesystem client support
-  DEPENDS:=+kmod-fs-nfs +LINUX_5_15:kmod-fs-nfs-ssc
+  DEPENDS:=+kmod-fs-nfs +!LINUX_5_4:kmod-fs-nfs-ssc
   KCONFIG:= \
 	CONFIG_NFS_V4=y
   FILES:= \
