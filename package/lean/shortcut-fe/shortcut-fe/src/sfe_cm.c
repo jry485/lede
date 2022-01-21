9483b76a6475ff7768c891348d1645ef556aa4ce
W_Y_CPP <34025388+WYC-2020@users.noreply.github.com>
2022-01-20 18:10:11 -0600

    shortcut-fe:fix unused param

--- package/lean/shortcut-fe/shortcut-fe/src/sfe_cm.c
+++ package/lean/shortcut-fe/shortcut-fe/src/sfe_cm.c
@@ -311,8 +311,9 @@ static unsigned int sfe_cm_post_routing(struct sk_buff *skb, int is_v4)
 	struct nf_conntrack_tuple reply_tuple;
 	struct sk_buff *tmp_skb = NULL;
 	SFE_NF_CONN_ACCT(acct);
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 0)
 	struct net *net=NULL;
-
+#endif
 	/*
 	 * Don't process broadcast or multicast packets.
 	 */
