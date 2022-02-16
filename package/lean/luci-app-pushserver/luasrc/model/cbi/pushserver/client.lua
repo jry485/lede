f8b37ecef6afba72601c03f6ed347272e166fcc6
W_Y_CPP <383152993@qq.com>
2022-02-16 00:17:35 -0500

    packages:move package to luci

--- package/lean/luci-app-pushserver/luasrc/model/cbi/pushserver/client.lua
+++
@@ -1,6 +0,0 @@
-f = SimpleForm("pushserver")
-luci.sys.call("/usr/bin/pushserver/pushserver client")
-f.reset = false
-f.submit = false
-f:append(Template("pushserver/client"))
-return f
--- package/lean/luci-app-pushserver/luasrc/model/cbi/pushserver/log.lua
+++
@@ -1,5 +0,0 @@
-f = SimpleForm("pushserver")
-f.reset = false
-f.submit = false
-f:append(Template("pushserver/log"))
-return f
